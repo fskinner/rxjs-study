32d2bf68dd85d59ea75a75ea87314475833f95d3
Felipe Skinner <felipeskinner@gmail.com>
2016-04-30 14:31:42 -0300

    Add filter list example

--- src/index.js
+++ src/index.js
@@ -2,39 +2,27 @@ import Rx from 'Rx';
 
 const _input = $('.input');
 const _results = $('.results');
-const _resultDescription = $('.result-description');
+
+const people = [
+  {name: 'John', phone: '555-4321'},
+  {name: 'Johanna', phone: '555-8765'},
+  {name: 'Mario', phone: '555-1357'},
+  {name: 'Luigi', phone: '555-8264'},
+  {name: 'Zuirio', phone: '555-0091'}
+];
 
 const keyup$ = Rx.Observable.fromEvent(_input, 'keyup')
-  .map(ev => ev.target.value);
+  .map(ev => ev.target.value)
+  .debounce(500);
 
-const debouncedKeyup$ = keyup$.debounce(500);
-const distinct$ = debouncedKeyup$.distinctUntilChanged();
+const people$ = Rx.Observable.of(people)
+  .merge(keyup$)
+  .scan((list, value) => people.filter(item => item.name.includes(value)));
 
-function searchWikipedia(term) {
-  return $.ajax({
-    url: 'http://en.wikipedia.org/w/api.php',
-    dataType: 'jsonp',
-    data: {
-        action: 'opensearch',
-        format: 'json',
-        search: term
-    }
-  }).promise();
-}
-
-const suggestion$ = keyup$.flatMapLatest(searchWikipedia);
-
-suggestion$.subscribe(data => {
+people$.subscribe(data => {
   _results.empty();
-  _resultDescription.empty();
 
-  if(data[0] !== '') {
-    let res = data[1];
-    res.forEach(item => $(`<li class="list-item">${item}</li>`).appendTo(_results));
-
-    $('<h2> Brief description </h2>').appendTo(_resultDescription);
-    $(`<p>${data[2][0]}</p>`).appendTo(_resultDescription);
-  }
+  data.forEach(item => $(`<li>${item.name} - ${item.phone}</li>`).appendTo(_results))
 }, error => {
   _results.empty();
 
